c31a6a20fcba0ab84b23ed6973279a2b209346d0
norian <160144830+nnorian@users.noreply.github.com>
2024-05-28 23:54:39 +0300

    prezice cheltuielile potentiale
    in baza datelor despre 36 luni precedente presupune cheltuielile din luna curenta

--- ml_expenses_prediction.py
+++ ml_expenses_prediction.py
@@ -13,8 +13,6 @@ X = data[['Food', 'Transport', 'Studies', 'Fun', 'Weird Stuff']]
 y = data['Budget']
 
 
-
-
 # разделение датасетов 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
